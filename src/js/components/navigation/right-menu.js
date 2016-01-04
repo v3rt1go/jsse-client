68e1d53e8f02532b93dc4d88b2bfa8f7f91e66f8
Alex Griciuc <agriciuc@outlook.com>
2016-01-04 19:08:40 +0200

    tests on right main menu

--- src/js/components/navigation/right-menu.js
+++ src/js/components/navigation/right-menu.js
@@ -5,11 +5,14 @@ const LeftMenu = () => {
   return (
     <div className="right menu">
       <div className="item">
-        <div className="ui icon positive button" tabIndex="0">
+        <div className="ui icon teal button" tabIndex="0">
           <div className="ui tiny floating circular red label">5</div>
           <i className="cart icon"></i>
           {/* TODO: Default text for no items in cart is: COSUL MEU */}
           <span className="cart-total">423 RON</span>
+          {/* TODO: Add a submenu with all the items from the cart and total.
+              Should be displayed when Cart button is hovered
+          */}
         </div>
       </div>
     </div>
